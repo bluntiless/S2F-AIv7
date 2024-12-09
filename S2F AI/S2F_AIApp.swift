e3dc6ffbc86a039fe8f2521579c7b40b73c990ed
bluntiless <waynezilla@hotmail.co.uk>
2024-12-09 22:39:01 +0000

    Initial Commit

---
+++ S2F AI/S2F_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  S2F_AIApp.swift
+//  S2F AI
+//
+//  Created by WAYNE WRIGHT on 09/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct S2F_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ S2F AITests/S2F_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  S2F_AITests.swift
+//  S2F AITests
+//
+//  Created by WAYNE WRIGHT on 09/12/2024.
+//
+
+import Testing
+@testable import S2F_AI
+
+struct S2F_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
